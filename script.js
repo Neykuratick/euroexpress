1c83c499aebdfccef82a7f1659d106c38f325976
Neykuratick <neykuratick@yahoo.com>
2021-01-13 20:06:54 +0300

    cords finder

--- script.js
+++ script.js
@@ -36,9 +36,9 @@ function next(direction){
 
 }
 
-
 ymaps.ready(init);
-    function init(){
+var coordinates;
+function init() {
      var myMap = new ymaps.Map("map", {
           center: [55.76, 37.64],
           zoom: 10,
@@ -50,6 +50,33 @@ ymaps.ready(init);
      myMap.controls.add('typeSelector');
 
 
-     myMap.IEventManager()
+     var searchControl = myMap.controls.get('searchControl');
 
-     }
+     searchControl.events.add('submit', function (e) {
+          // console.log(searchControl.getResultsArray());
+     }, this);
+
+     searchControl.events.add("resultselect", function (e) {
+               console.log("grege");
+               var resString = searchControl.getRequestString();
+               console.log(resString);
+               var myGeocoder = ymaps.geocode(resString);
+               myGeocoder.then(
+                    function (res) {
+                         // Выведем в консоль данные, полученные в результате геокодирования объекта.
+                              var cords = res.geoObjects.get(0).properties.get("boundedBy")[0];
+                              var cordss = res.geoObjects.get(0).properties.get("boundedBy")[1];
+                              var finalx = cords[0] + cordss[0]
+                              var finalx = finalx / 2;
+                              var finaly = cords[1] + cordss[1]
+                              var finaly = finaly / 2;
+                              coordinates = [finalx, finaly];
+                              console.log(coordinates)
+                         },
+                    function (err) {
+                         // Обработка ошибки.
+                    });
+          }
+     );
+
+}
