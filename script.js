e655f05b7b5154ea51053410438f206c61b8a865
Neykuratick <neykuratick@yahoo.com>
2021-01-14 21:44:57 +0300

    added search box

--- script.js
+++ script.js
@@ -41,6 +41,11 @@ var markers = [];
 var markersPosition = [];
 var distance;
 
+function initialize() {
+  initMap();
+  initAutocomplete();
+}
+
 let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -88,6 +93,98 @@ function initMap() {
     console.log("markers", markers)
 
   })
+}
+
+function initAutocomplete() {
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markersLocal = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markersLocal.
+    markersLocal.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markersLocal = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      // Create a marker for each place.
+      markersLocal.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      // --------------------------------------------------------------------------
+
+      
+      if (markers.length > 1) {
+        markers = [];
+        markersPosition = [];
+        distance = 0;
+        document.getElementById("point2").innerHTML = "Ваша вторая точка: ";
+        document.getElementById("distance").innerHTML = "Расстояние: ";
+      }
+  
+      markers.push(markersLocal[0])
+  
+      if (markers.length < 2) {
+        var marPos = markers[0].getPosition().toJSON()
+        markersPosition.push(jsonToArray(marPos))
+        
+        adress = geocode(markersPosition[0])
+        document.getElementById("point1").innerHTML = "Ваша первая точка: " + adress;
+      }
+  
+      if (markers.length == 2) {
+        var marPos = markers[1].getPosition().toJSON()
+        markersPosition.push(jsonToArray(marPos))
+  
+        adress = geocode(markersPosition[1])
+        document.getElementById("point2").innerHTML = "Ваша вторая точка: " + adress;
+  
+        marker1 = markers[0].getPosition()
+        marker2 = markers[1].getPosition()
+        distance = getDistance(marker1, marker2)
+        document.getElementById("distance").innerHTML = "Расстояние: " + roundNumber(distance, 0) + " метров";
+      }
+
+      // -------------------------------------------------------------------------
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
 
 function placeMarkerAndPanTo(latLng, map) {
